8356e38f04770af1c35af34d2e396df6a3f6c7b4
JobQ <qcm3771787@icloud.com>
2016-09-28 11:21:16 +0800

    this is a test

--- src/LeetCode/Problem3.java
+++ src/LeetCode/Problem3.java
@@ -1,13 +1,25 @@
 package LeetCode;
 
+import java.util.HashSet;
+
 /**
  * Created by JobQ on 9/26/2016.
  */
 public class Problem3 {
     public static void main(String[] args) {
         String test="abca";
+        HashSet<Character> hc = new HashSet<>();
+        hc.add('a');
+        hc.add('b');
+        System.out.println(hc.remove('b'));
     }
+    /**
+     * the most easy method, check one by one
+     * */
     public static int lengthOfLongestSubstring(String s){
+        int start =0;
+        int end = 1;
+
         return 1;
     }
 }
