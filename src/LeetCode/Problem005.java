99b8f32e6cc298569572c2b9ca98d1f887070971
JobQ <qcm3771787@icloud.com>
2016-09-29 13:57:27 +0800

    this is a test

--- src/LeetCode/Problem005.java
+++ src/LeetCode/Problem005.java
@@ -2,7 +2,18 @@ package LeetCode;
 
 /**
  * Created by JobQ on 9/29/2016.
- * Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ * Given a string S, find the longest palindromic
+ * substring in S. You may assume that the maximum
+ * length of S is 1000, and there exists one unique
+ * longest palindromic substring.
  */
 public class Problem005 {
+    public static void main(String[] args) {
+        String s="helloworld";
+        System.out.println(longestPalindrome(s));
+    }
+    public static String longestPalindrome(String s) {
+return "123";
+
+    }
 }
